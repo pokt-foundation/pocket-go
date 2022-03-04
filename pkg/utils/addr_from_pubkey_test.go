9ea8fe64aa9a59c58b2633f24fc60fb4908b0154
Pablo Ocampo <pablojocampogo@gmail.com>
2022-03-04 12:08:20 -0400

    feat: change tests to have var names inside tests

--- pkg/utils/addr_from_pubkey_test.go
+++ pkg/utils/addr_from_pubkey_test.go
@@ -6,15 +6,14 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	address = "b50a6e20d3733fb89631ae32385b3c85c533c560"
-)
-
 func TestGetAddressFromPublickey(t *testing.T) {
 	c := require.New(t)
+
+	publicKey := "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3"
+	expectedAddress := "b50a6e20d3733fb89631ae32385b3c85c533c560"
 
 	resultingAddress, err := GetAddressFromPublickey(publicKey)
 	c.NoError(err)
 
-	c.Equal(resultingAddress, address)
+	c.Equal(resultingAddress, expectedAddress)
 }
--- pkg/utils/public_key_from_private_test.go
+++ pkg/utils/public_key_from_private_test.go
@@ -6,13 +6,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	publicKey  = "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3"
-	privateKey = "1f8cbde30ef5a9db0a5a9d5eb40536fc9defc318b8581d543808b7504e0902bcb243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3"
-)
-
 func TestPublicKeyFromPrivate(t *testing.T) {
 	c := require.New(t)
 
-	c.Equal(publicKey, PublicKeyFromPrivate(privateKey))
+	privateKey := "1f8cbde30ef5a9db0a5a9d5eb40536fc9defc318b8581d543808b7504e0902bcb243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3"
+	expectedPublicKey := "b243b27bc9fbe5580457a46370ae5f03a6f6753633e51efdaf2cf534fdc26cc3"
+
+	c.Equal(expectedPublicKey, PublicKeyFromPrivate(privateKey))
 }
