147c3da72fdf29704fc8add21708f832ff083393
Pablo Ocampo <pablojocampogo@gmail.com>
2022-03-08 23:44:14 -0400

    feat: implement client and mock client packages

---
+++ internal/client/client.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"time"
+
+	"github.com/gojektech/heimdall/httpclient"
+)
+
+const (
+	defaultHTTPClientTimeout = 5 * time.Second
+	defaultHTTPClientRetries = 3
+)
+
+type Client struct {
+	*httpclient.Client
+}
+
+// NewDefaultClient returns httpclient instance with default config
+func NewDefaultClient() *Client {
+	return &Client{
+		Client: httpclient.NewClient(
+			httpclient.WithHTTPTimeout(defaultHTTPClientTimeout),
+			httpclient.WithRetryCount(defaultHTTPClientRetries),
+		),
+	}
+}
+
+// NewCustomClient returns httpclient instance with given custom config
+func NewCustomClient(retries int, timeout time.Duration) *Client {
+	return &Client{
+		Client: httpclient.NewClient(
+			httpclient.WithHTTPTimeout(timeout),
+			httpclient.WithRetryCount(retries),
+		),
+	}
+}
