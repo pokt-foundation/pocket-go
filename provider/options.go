758a37d341453349156fd1f2e33c930ac95d12fc
Pablo Ocampo <pablojocampogo@gmail.com>
2022-04-25 18:09:09 -0400

    refactor: clean provider pkg code

--- provider/options.go
+++ provider/options.go
@@ -1,5 +1,37 @@
 package provider
 
+// JailedStatus enum that represents jailed status
+type JailedStatus int
+
+const (
+	// Jailed status is when a node has been jailed due to missing a determined amount of blocks and/or byzantine behavior and thus cannot serve relays nor participate in consensus
+	Jailed JailedStatus = iota + 1
+	// Unjailed status is when a node is not jailed and thus can serve relays
+	Unjailed
+)
+
+// StakingStatus enum that represents staking status
+type StakingStatus int
+
+const (
+	// Unstaked represents unstaked status
+	Unstaked StakingStatus = iota
+	// Unstaking represents unstaking status
+	Unstaking
+	// Staked represents staked status
+	Staked
+)
+
+// Order enum that represents the order which RPC requests should return their outputs
+type Order string
+
+const (
+	// DescendantOrder represents greater to lower order
+	DescendantOrder Order = "desc"
+	// AscendantOrder represents lower to greater order
+	AscendantOrder Order = "asc"
+)
+
 // GetBalanceOptions represents optional arguments for GetBalance request
 type GetBalanceOptions struct {
 	Height int
